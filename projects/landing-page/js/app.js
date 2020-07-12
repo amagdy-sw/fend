8ba3d201af25a084ebcf6e79a90ef5c857d60693
amagdy-sw <68121587+amagdy-sw@users.noreply.github.com>
2020-07-12 19:33:36 +0200

    landing project completion
    landing project completion

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,7 +17,18 @@
  * Define Global Variables
  * 
 */
+//main container element which contains sections
+const main = document.querySelector('main');
 
+//sections nodelist
+const sections = document.querySelectorAll("main section");
+
+//navigation menu ul
+const navmenu = document.getElementById('navbar__list');
+
+//variables for window scroll event
+let last_known_scroll_position = 0;
+let ticking = false;
 
 /**
  * End Global Variables
@@ -25,21 +36,69 @@
  * 
 */
 
-
-
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 
-// build the nav
 
+// build the nav
+function BuildNavigationMenu(){
+  let i = 0;
+    for(let section of sections){
+        let a = document.createElement('a');
+        a.textContent = section.querySelector('h2').textContent;
+        a.id = `a${section.id}`;
+        a.href=`#${section.id}`;
+        if(i==0){
+          a.className ="menu__link menu_link_active";
+        }else{
+          a.className = "menu__link";
+        }
+        // Scroll to anchor ID using scrollTO event
+        // Scroll to section on link click
+        a.addEventListener('click', function(event){
+          let sectionId = a.id.slice(1);
+          document.querySelector(`#${sectionId}`).scrollIntoView({behavior: "smooth"});
+          event.preventDefault();
+        });
+        let li = document.createElement('li');
+        li.appendChild(a);
+        navmenu.appendChild(li);
+        i++;
+    }
+}
 
 // Add class 'active' to section when near top of viewport
 
+// The checker
+function isScrolledIntoView(el) {
+  let
+  rect = el.getBoundingClientRect(),
+  windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+  //see if the section is partially visible with 60% because of margins and padding
+  return !(
+    Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-(rect.height / 1)) * 100)) < 60 ||
+    Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < 60
+  )
+}
 
-// Scroll to anchor ID using scrollTO event
+// Usage.
+function setSetionActiveClass(){
+  for(let section of sections){
+    section.classList.remove("active-class");  
+    document.querySelector(`#a${section.id}`).classList.remove("menu_link_active");
+  }
+  for(let section of sections){
+      if(isScrolledIntoView(section)){
+          section.classList.add("active-class");
+          document.querySelector(`#a${section.id}`).classList.add("menu_link_active");
+          break;
+      }
+  }
+}
+
 
 
 /**
@@ -49,9 +108,17 @@
 */
 
 // Build menu 
-
-// Scroll to section on link click
+BuildNavigationMenu();
 
 // Set sections as active
-
+window.addEventListener('scroll', function(e) {
+    last_known_scroll_position = window.scrollY;
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        setSetionActiveClass();
+        ticking = false;
+      });  
+      ticking = true;
+    }
+});
 
